6729436e721031775a8277b3940c69ed7c6cca50
Steven <stevenbmendoza@gmail.com>
2022-04-22 13:25:57 -0700

    folder struct, npm dependcies, npm start, env config, sequilize init, sql schema

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+// import the sequilize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create conneciton to our database, pass in your 
+// MYSQL information for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
