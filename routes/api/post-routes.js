feafaddf1bde027177ed9c35955116d911fa6464
StevenM <stevenbmendoza@gmail.com>
2022-04-27 18:55:34 -0700

    update post title with put

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -41,4 +41,38 @@ router.get('/:id', (req, res) => {
   });
 });
 
+//create a post route
+router.post('/', (req, res) => {
+   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  Post.create({
+    title: req.body.title,
+    post_url: req.body.post_url,
+    user_id: req.body.user_id
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// update a post title
+router.put('/:id', (req, res) => {
+  Post.update(
+    { title: req.body.title },
+    { where: { id: req.params.id }}
+  )
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 module.exports = router;
