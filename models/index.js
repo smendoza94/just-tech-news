3eb7f47658379f5142cfec838b446a0e38274369
StevenM <stevenbmendoza@gmail.com>
2022-04-28 16:56:23 -0700

    finish Vote model, added to indexjs model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create a "one to many relation" user to post association
 User.hasMany(Post, {
@@ -11,4 +12,4 @@ Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
