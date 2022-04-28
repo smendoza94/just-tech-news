8aa601723593d49282eb596f730aa6d957726339
StevenM <stevenbmendoza@gmail.com>
2022-04-27 18:33:21 -0700

    get all posts and one id post routes

---
+++ routes/api/post-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// get all users posts
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+    // query config
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// get a single post
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ['id','post_url','title','created_at'],
+    include: [{ model: User, attributes: ['username'] }]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false })
+
+// leave the .sync({ force: false }) so it doesnt erase the data base
+// on every start up of the server
+
+// set .sync({ force: true }) to true the database connection must sync 
+// with the model definitions and associations. By forcing the sync method 
+// to true, we will make the tables re-create if there are any association changes.
+sequelize.sync({ force: false }) // force: true = DROP TABLE IF EXISTS
   .then(() => {
     app.listen(PORT, () => console.log('Now listening...'));
   });
