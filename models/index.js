2455a72ca28b8dc09ae30c2ee5b9ae5aaeb81a7c
StevenM <stevenbmendoza@gmail.com>
2022-04-27 17:01:42 -0700

    create associations between User and Post models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create a "one to many relation" user to post association
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+// create a "one to one relation" post to user association
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
