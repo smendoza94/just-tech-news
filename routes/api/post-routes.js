2bbda1d77ee74ee3dd69c201feedf4acac41e82e
Steven <stevenbmendoza@gmail.com>
2022-04-29 16:59:59 -0700

    vote put Post.upvote method replaced code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -80,29 +80,8 @@ router.post('/', (req, res) => {
 // must be placed before the put /:id Express.js will think the word "upvote" is a valid parameter for /:id
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-  // create the vote
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  })  
-  .then(() => {
-    // then find the post we just voted on
-    return Post.findOne({
-      where: { id: req.body.post_id },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        // use raw MySQL aggregate function query to get a count of how many 
-        // votes the post has and return it under the name `vote_count`
-        [
-          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-          'vote_count'
-        ]
-      ]
-    })
-  })  
+  // custom static method create in models/Post.js
+  Post.upvote(req.body, { Vote })  
   .then(dbPostData => res.json(dbPostData))
   .catch(err => {console.log(err); res.status(400).json(err)});
 });
