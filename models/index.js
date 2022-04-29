0bf19e14646edc672be3bbcbf321fd53b70a51fe
StevenM <stevenbmendoza@gmail.com>
2022-04-28 17:06:31 -0700

    vote assocation bewteen User and Posts

--- models/index.js
+++ models/index.js
@@ -2,14 +2,27 @@ const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
 
-// create a "one to many relation" user to post association
+// create a "one to many relation" User to Post association
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
-// create a "one to one relation" post to user association
+// create a "one to one relation" Post to User association
 Post.belongsTo(User, {
   foreignKey: 'user_id',
+});
+
+// "many to many relation" User to Votes to Posts- user can vote many times and votes can belong to many posts
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Vote };
