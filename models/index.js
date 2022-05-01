a4b7dcce3adbc01714242d86eb06111e1ff2daff
StevenM <stevenbmendoza@gmail.com>
2022-04-30 21:31:50 -0700

    comment associations with posts and users

--- models/index.js
+++ models/index.js
@@ -45,4 +45,21 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
+// Comment associations
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+})
+
 module.exports = { User, Post, Vote };
