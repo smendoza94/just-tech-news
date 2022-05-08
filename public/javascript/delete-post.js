21bbeb4ecc0d47dfb3d7092ccf34ad85d7d25f4d
Steven <stevenbmendoza@gmail.com>
2022-05-08 10:50:13 -0700

    edit posts can now change titles of posts js logic

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,20 +1,21 @@
 async function deleteFormHandler(event) {
   event.preventDefault();
-
-  const id = window.location.toString().split("/")[
+  // take the post id from the url
+  const post_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
-  const response = await fetch(`/api/posts/${id}`, {
+  // use the post id to run the delete route from post
+  const response = await fetch(`/api/posts/${post_id}`, {
     method: "DELETE",
   });
-
+  // once deleted take user back to dashboard page
   if (response.ok) {
     document.location.replace("/dashboard/");
   } else {
     alert(response.statusText);
   }
 }
-
+// js logic to add "click" then run function
 document
   .querySelector(".delete-post-btn")
   .addEventListener("click", deleteFormHandler);
