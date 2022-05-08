98020377f318bdfc211893ee78d3d19f31d4b208
Steven <stevenbmendoza@gmail.com>
2022-05-07 23:18:13 -0700

    format url's to shorthand function and jest tested

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,4 +1,4 @@
-const { format_date, format_plural } = require("../utils/helpers");
+const { format_date, format_plural, format_url } = require("../utils/helpers");
 
 test("format_date() returns a date string", () => {
   const date = new Date("2020-03-20 16:12:03");
@@ -13,4 +13,14 @@ test("format_plural returns a singular or plural word string", () => {
 
   expect(format_plural(animalOne, animalOneAmount)).toBe("Lion");
   expect(format_plural(animalTwo, animalTwoAmount)).toBe("Tigers");
+});
+
+test("format_url() returns a simplified url string", () => {
+  const url1 = format_url("http://test.com/page/1");
+  const url2 = format_url("https://www.coolstuff.com/abcdefg/");
+  const url3 = format_url("https://www.google.com?q=hello");
+
+  expect(url1).toBe("test.com");
+  expect(url2).toBe("coolstuff.com");
+  expect(url3).toBe("google.com");
 });
